06ce6fa9ebafa9209e8920ce7a3ae4ead3ff38c4
fatmouch <dahenane.fatma@gmail.com>
2023-10-19 12:53:51 -0700

    00

--- personal_print.c
+++
@@ -1,8 +0,0 @@
-#include "shell.h"
-
-/* my own personal print func */
-
-void tima_mhmd_print_function(const char *missage)
-{
-write(STDOUT_FILENO, missage, strlen(missage)); /*personal print*/
-}
